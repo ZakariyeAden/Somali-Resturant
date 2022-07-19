bb4ab90d8cb16e6fde00a6fbc9131c0b0e49801c
ZakariyeAden <zakariye5417@gmail>
2022-07-19 08:55:03 -0500

    Menu page

--- src/Components/Pages/Menu.js
+++ src/Components/Pages/Menu.js
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import "../../Style/Menu.css";
+import PlateSelection from "../MenuSelections/PlateSelection";
+import Halal from "../../Assets/halal.jpg";
+import { Parallax } from "react-parallax";
+import AppetizerSelection from "../MenuSelections/AppetizerSelection";
+import DesertSelection from "../MenuSelections/DesertSelection";
+const style= {
+  margin: "0 0 10rem 0",
+};
+const insideStyles = {
+  padding: 20,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%,-50%)",
+};
 function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <section>
+      <Parallax bgImage={Halal}>
+        <div style={{ height: 500 }}>
+          <div style={insideStyles} className="menu-heading">
+            Menu
+          </div>
+        </div>
+      </Parallax>
+    <PlateSelection style={style}/>
+    <AppetizerSelection style={style}/>
+    <DesertSelection style={style}/>
+    </section>
+  );
 }
 
-export default Menu
+export default Menu;
