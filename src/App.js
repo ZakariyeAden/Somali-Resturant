ddc094da41fb3b08fc66fd2f28745711991ce426
ZakariyeAden <zakariye5417@gmail>
2022-07-19 09:01:53 -0500

    Added routing and components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Home from "./Components/Pages/Home";
 import About from "./Components/Pages/About";
 import Menu from "./Components/Pages/Menu";
 import Contact from "./Components/Pages/Contact";
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Nav/Navbar";
+import Footer from './Components/Footer/Footer'
 import { Routes,Route } from 'react-router-dom'
+
 function App() {
   return (
     <div>
@@ -15,6 +17,7 @@ function App() {
           <Route path="/menu" element={<Menu />} />
           <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
