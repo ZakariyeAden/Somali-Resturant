fd47fd4316b6f88cfbb5bdd6b1d2a80bcddae53b
ZakariyeAden <zakariye5417@gmail>
2022-07-20 12:10:27 -0500

    _

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React,{Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+import 'animate.css';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
+    <Suspense fallback={<p className="test">Loading...</p>}>
       <App />
+      </Suspense>
     </React.StrictMode>
   </BrowserRouter>
 );
