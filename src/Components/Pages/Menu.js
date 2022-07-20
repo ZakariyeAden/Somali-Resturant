ee459e3cb69dd97b2d196b1415ec72294754aea5
ZakariyeAden <zakariye5417@gmail>
2022-07-20 12:15:38 -0500

    updating the menu

--- src/Components/Pages/Menu.js
+++ src/Components/Pages/Menu.js
@@ -4,6 +4,7 @@ import PlateSelection from "../MenuSelections/PlateSelection";
 import Halal from "../../Assets/halal.jpg";
 import { Parallax } from "react-parallax";
 import AppetizerSelection from "../MenuSelections/AppetizerSelection";
+
 import DesertSelection from "../MenuSelections/DesertSelection";
 const style= {
   margin: "0 0 10rem 0",
@@ -20,7 +21,7 @@ function Menu() {
     <section>
       <Parallax bgImage={Halal}>
         <div style={{ height: 500 }}>
-          <div style={insideStyles} className="heading-primary">
+          <div style={insideStyles} className="heading-primary animate__animated ">
             Menu
           </div>
         </div>
