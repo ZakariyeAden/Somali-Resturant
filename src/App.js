8232e63a7e48a76600c6fe4a86089b3f1be42ba6
ZakariyeAden <zakariye5417@gmail>
2022-07-20 12:10:55 -0500

    adding lazy loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import Home from "./Components/Pages/Home";
 import About from "./Components/Pages/About";
 import Menu from "./Components/Pages/Menu";
 import Contact from "./Components/Pages/Contact";
 import Navbar from "./Components/Nav/Navbar";
 import Footer from './Components/Footer/Footer'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
+const MainComponent = lazy(() => import("./Components/Component"));
 
-function App() {
+function App(props) {
   return (
     <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer/>
+        </Routes>
+        <MainComponent />
+        <Footer />
+      </Suspense>
     </div>
   );
 }
